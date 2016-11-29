056cf2248b63ab58eb00e424d59288146faf83e1
joshuajhun <joshuajhun@gmail.com>
2016-11-29 08:48:32 -0700

    added es6

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,28 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: "./lib/index.js",
-    test: "mocha!./test/sample.js"
+    main: [ 'babel-polyfill', './lib/index.js'],
+    test: ['babel-polyfill','mocha!./test/sample.js']
   },
   output: {
     path: __dirname,
     filename: "[name].bundle.js"
-  }
-}
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: 'es2015',
+        },
+      },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!sass' },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json', '.scss', '.css'],
+  },
+};
